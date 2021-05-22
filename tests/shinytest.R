c001a9aff2f25e243feb6eab399c1fe8740615a6
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-05-22 16:38:44 -0400

    testing goign well

--- tests/shinytest.R
+++ tests/shinytest.R
@@ -1,3 +1,5 @@
+# This script calls the testing script. 
+
 library(shinytest)
 shinytest::testApp(here::here())
 
--- tests/shinytest/mytest.R
+++ tests/shinytest/mytest.R
@@ -1,3 +1,15 @@
+###########
+
+# This script runs each response from the observed 24 participants through the app
+# right now its not using key presses, but a radio button and action button
+# because I can't figure out how to input the key presses in the unit test. 
+# the inputs are the same otherwise though. 
+# it returns a .csv file for each participant and saves it in the tests tests_output folder
+# there is another script called "process_test_data.R"
+# that processes the output of this unit test. 
+
+## !!!! Call this script using shinytest.R 
+
 
 library(here)
 library(tidyverse)
@@ -13,25 +25,34 @@ observed <- read_csv(here("validation", "validation.csv")) %>%
 examinees <- unique(observed$examinee)
 
 for(i in examinees){
+# start the test
 app <- ShinyDriver$new(here())
+# give it a name
 app$snapshotInit("mytest")
-
+# name is the examinee name
 app$setInputs(name = i)
+# ntoes is just notes. 
 app$setInputs(notes = "notes")
+# 30 items
 app$setInputs(numitems = "30")
+# the date on teh unit test is a little funky. I've just left it
+# it doesn't matter. its not tracked
 #app$setInputs(date = 18769)
+
+# click to get started
 app$setInputs(start = "click")
 
 # what responses do you want
+# subsets the observed responses in order
 responses <- observed$response[observed$examinee==i]
 
-  # key presses
+  # key presses (fed to a radio button and action button for now)
   for(p in 1:length(responses)){
     app$setInputs(keys = responses[p])
     app$setInputs(enter_key = "click")
   }
 
-#app$snapshot()
+# grabs the values at the end of the test. 
 exports <- app$getAllValues()[[3]]
 df <- tibble(
   discrimination = strsplit(exports$discrimination, "_", 1)[[1]],
@@ -45,6 +66,7 @@ df <- tibble(
   sem = strsplit(exports$sem, "_")[[1]],
   name = i
 )
+# save the .csv file
 write.csv(df, here("tests", "test_output", paste0(i, "_test_dat.csv")))
 }
 
