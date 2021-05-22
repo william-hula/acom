f07b476f677aa617a5fc14d170f54caf1aa517c8
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-05-22 16:11:06 -0400

    unit testing BOOM. now figure out key presses so it can be in master

--- tests/process_test_data.R
+++ tests/process_test_data.R
@@ -1,20 +1,40 @@
 # processing ability out
+library(fs)
+library(vroom)
+files <- fs::dir_ls(here("tests", "test_output"))
+df = vroom::vroom(files)
+rm(files)
 
-test_dat <- readRDS(here("tests", "test_dat.RDS"))
+shiny <- df %>%
+  select(examinee=name, order, target, discrimination, itemDifficulty, response, ability, error = sem)
+# observed data
+observed <- read.csv(here("validation", "validation.csv")) %>%
+  # only need rows with testing
+  filter(modelType == "3pl",
+         exam == "PNT-CAT30") %>%
+  mutate(response = ifelse(response == "correct", 0, 1)) %>%
+  hablar::retype() %>%
+  group_by(examinee) %>%
+  mutate(order = row_number()) %>%
+  ungroup() %>%
+  select(examinee, order, target = item, discrimination, itemDifficulty = difficulty, response, ability, error) %>%
+  # identify the order to select last item later on. 
+  rename_with(~str_c("obs_", .), 3:8)
 
-exports <- test_dat[[3]]
-
-df <- tibble(
-  discrimination = strsplit(exports$discrimination, "_", 1)[[1]],
-  target = strsplit(exports$words, "_")[[1]],
-  item_number = strsplit(exports$item_number, "_")[[1]],
-  order = strsplit(exports$order, "_")[[1]],
-  itemDifficulty = strsplit(exports$itemDifficulty, "_")[[1]],
-  key_press = strsplit(exports$key_press, "_")[[1]],
-  response = strsplit(exports$responses, "_")[[1]],
-  ability = strsplit(exports$abil, "_", 1)[[1]],
-  sem = strsplit(exports$sem, "_")[[1]]
-)
+together <- observed %>%
+  full_join(shiny, by = c("examinee", "order")) %>%
+  mutate(target_test = ifelse(obs_target==target, 1, 0),
+         ability_test = obs_ability - ability,
+         error_test = obs_error - error,
+         difficulty_test = obs_itemDifficulty - itemDifficulty)
 
 
+hist(together$ability_test, breaks = 20)
+hist(together$error_test, breaks = 20)  
+
+final <- together %>%
+  filter(order == 30)
+
+cor(final$ability, final$obs_ability)
+plot(final$ability, final$obs_ability)
 
--- tests/shinytest/mytest.R
+++ tests/shinytest/mytest.R
@@ -1,20 +1,53 @@
-app <- ShinyDriver$new("../../")
+
+library(here)
+library(tidyverse)
+
+# observed data
+observed <- read_csv(here("validation", "validation.csv")) %>%
+  # only need rows with testing
+  filter(modelType == "3pl",
+         exam == "PNT-CAT30") %>%
+  mutate(response = ifelse(response == "correct", "2", "1")) %>%
+  select(examinee, response)
+
+examinees <- unique(observed$examinee)
+
+for(i in examinees){
+app <- ShinyDriver$new(here())
 app$snapshotInit("mytest")
 
-app$setInputs(name = "name")
+app$setInputs(name = i)
 app$setInputs(notes = "notes")
-app$setInputs(date = 18769)
+app$setInputs(numitems = "30")
+#app$setInputs(date = 18769)
 app$setInputs(start = "click")
 
 # what responses do you want
-responses <- rep(c("1", "2"), 5)
+responses <- observed$response[observed$examinee==i]
 
-# key presses
-for(i in 1:length(responses)){
-  app$setInputs(keys = responses[i])
-  app$setInputs(enter_key = "click")
+  # key presses
+  for(p in 1:length(responses)){
+    app$setInputs(keys = responses[p])
+    app$setInputs(enter_key = "click")
+  }
+
+#app$snapshot()
+exports <- app$getAllValues()[[3]]
+df <- tibble(
+  discrimination = strsplit(exports$discrimination, "_", 1)[[1]],
+  target = strsplit(exports$words, "_")[[1]],
+  item_number = strsplit(exports$item_number, "_")[[1]],
+  order = strsplit(exports$order, "_")[[1]],
+  itemDifficulty = strsplit(exports$itemDifficulty, "_")[[1]],
+  key_press = strsplit(exports$key_press, "_")[[1]],
+  response = strsplit(exports$responses, "_")[[1]],
+  ability = strsplit(exports$abil, "_", 1)[[1]],
+  sem = strsplit(exports$sem, "_")[[1]],
+  name = i
+)
+write.csv(df, here("tests", "test_output", paste0(i, "_test_dat.csv")))
 }
-app$snapshot()
-vals <- app$getAllValues()
-saveRDS(vals, file = here("tests", "test_dat.RDS"))
+
+
+
 
