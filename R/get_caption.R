339bf81bf53ccf58a6066829de82638183fa8547
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2022-06-13 19:40:41 -0400

    typo in results caption fixed

--- R/get_caption.R
+++ R/get_caption.R
@@ -18,11 +18,11 @@ get_caption <- function(repeat_admin){
   
   if(!isTruthy(repeat_admin)){
     
-   cap = "The blue dashed line reflects current estimate and the shaded area reflects uncertainty in current estimate. The average ability for individuals with aphasia is 50, with a standard deviation of 10. The density plot represents the distribution of ability scores in the calibration sample, which includes 296 cases from the Moss Aphasia Psycholinguistic Project Database and 47 cases from NIH/NIDCD Award R03DC014556 (PI: Fergadiotis)"
+   cap = "The blue dashed line reflects the current estimate and the shaded area reflects uncertainty in the current estimate. The average ability for individuals with aphasia is 50, with a standard deviation of 10. The density plot represents the distribution of ability scores in the calibration sample, which includes 296 cases from the Moss Aphasia Psycholinguistic Project Database and 47 cases from NIH/NIDCD Award R03DC014556 (PI: Fergadiotis)"
     
   } else {
     
-   cap = "The blue dashed line reflects current estimate and the red dashed line reflects the estimate from the previous test. Shaded areas reflects uncertainty in the estiates. The average ability for individuals with aphasia is 50, with a standard deviation of 10. The density plot represents the distribution of ability scores in the calibration sample, which includes 296 cases from the Moss Aphasia Psycholinguistic Project Database and 47 cases from NIH/NIDCD Award R03DC014556 (PI: Fergadiotis)"
+   cap = "The blue dashed line reflects the current estimate and the red dashed line reflects the estimate from the previous test. Shaded areas reflects uncertainty in the estiates. The average ability for individuals with aphasia is 50, with a standard deviation of 10. The density plot represents the distribution of ability scores in the calibration sample, which includes 296 cases from the Moss Aphasia Psycholinguistic Project Database and 47 cases from NIH/NIDCD Award R03DC014556 (PI: Fergadiotis)"
   }
   #cat("\n",cap)
   return(cap)
